803642f38fe2013301f632638b0e2442a830b8f0
Ryan <ryan.bg.watson@gmail.com>
2019-02-12 12:26:31 -0500

    Added edit functionality for db

--- routes/contact.js
+++ routes/contact.js
@@ -46,4 +46,44 @@ router.post('/add', (req, res, next) => {
     });
 });
 
+/* GET Route for the Edit page
+    this will display the Edit page */
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+    contactModel.findById(id, (err, contactObject) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // show the edit view
+            res.render('contacts/edit', {
+                title: 'Edit Contact',
+                contact: contactObject
+            });
+        }
+    });
+});
+
+/* POST route for processing the Edit page */
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.update({ _id: id }, updatedContact, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // Refresh contact list
+            res.redirect('/contact-list');
+        }
+    });
+});
+
 module.exports = router;
