617dcddaa211f806598eaa121624cff2ee93a52b
Ryan <ryan.bg.watson@gmail.com>
2019-02-12 11:36:47 -0500

    Added create functionality to db

--- models/contact.js
+++ models/contact.js
@@ -9,4 +9,4 @@ let contactSchema = mongoose.Schema({
     collection: "first"
 });
 
-module.exports = mongoose.model('test', contactSchema);
+module.exports = mongoose.model('contact', contactSchema);
